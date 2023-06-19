ad3830225400704987ecf3a005642de3dbfc27cd
Silvano Catinella <catinella@yahoo.com>
2023-06-19 17:19:53 +0200

    First completed but not tested release

--- firmware/mbElectricalSystem.c
+++ firmware/mbElectricalSystem.c
@@ -62,12 +62,19 @@
 
 
 #include <xc.h>
+#include <stdlib.h>
+
 
 //
 // PINs declaration
 //
 #define _i_VX1          A1
 #define _i_VX2          A2
+#define _i_VY1          A3
+#define _i_VY2          A4
+// === availablre ===   A5
+// === availablre ===   A6
+#define _o_ENGINEOFF    A7
 #define _i_NEUTRAL      A8
 
 #define _i_LEFTARROW    B1
@@ -83,9 +90,10 @@
 #define _i_STARTBUTTON  C2
 #define _i_ENGINEON     C3
 #define _i_DECOMPRESS   C4
-#define _o_STOPLIGHT    C5
-#define _i_ADDLIGHT     C6
-#define _i_LIGHTONOFF   C7
+#define _i_POWEROFF     C5
+#define _o_STOPLIGHT    C6
+#define _i_ADDLIGHT     C7
+#define _i_LIGHTONOFF   C8
 
 #define _o_RIGHTARROW   D1
 #define _o_LEFTARROW    D2
@@ -97,25 +105,71 @@
 #define _o_STARTENGINE  D8
 
 
-
+#define BLINK_DELAY     4000000
 #define V_TOLERANCE     100
+#define BUTTON_DEBOUNC  10000
+
+uint16_t ADC_read(uint8_t channel) {
+	//
+	// Description:
+	//	It allows you to read the voltage value of the argument defined PIN
+	//
+	uint16_t out = 0;
+
+	ADCON0bits.CHS = channel;     // Seleziona il canale di ingresso analogico
+	ADCON0bits.GO = 1;            // Avvia la conversione
+
+	while (ADCON0bits.GO);        // Attendi il completamento della conversione
+	out = (ADRESH << 8) | ADRESL;
+
+	return(out);
+}
 
 
+uint8_t blink() {
+	//
+	// Description:
+	//	It returns 1 or 0 alternating after a given time
+	//
+	static uint8_t  status = 0;
+	static uint32_t counter = 0;
 
-uint8_t loop = 1;
+	if (counter >= BLINK_DELAY) {
+		if (status == 0) status = 1;
+		else             status = 0;
+		counter = 0;
+	} else
+		counter++;
+
+	return(status);
+}
+
+
 //-------------------------------------------------------------------------------------------------------------------------------
 //                                                      M A I N
 //-------------------------------------------------------------------------------------------------------------------------------
 
 void main(void) {
-	
+	uint8_t  loop                  = 1; // It enables the main loop (Just for future applications)
+	uint8_t  ready_flag            = 0; // When the flag is true (1), the motorbike is ready to accept commands
+	uint8_t  decompress_flag       = 0; // It is set when the decompressor lever has been pulled
+	uint8_t  engstart_flag         = 1; // It means the driver is pushing the start button and the electric engine is running
+	uint16_t startButtonDebouncing = 0; // It is the counter used to manage the start button bebouncing event
+
 	//
-	// Data Direction Registers (DDR) setting
+	// Data Direction Registers (DDR) and analog setting
 	//	TRISBbits.TRIS<PIN> = 0;  // PIN is set as output
 	//	TRISBbits.TRIS<PIN> = 1;  // PIN is set as input
+	//	ANSELbits.ANS<PIN> = 1:   // PIN is set as analog input
 	//
+
 	// Port A
-	TRISBbits.TRIS_i_NEUTRAL = 1;
+	ANSELbits.ANS_i_VX1          = 1;
+	ANSELbits.ANS_i_VX2          = 1;
+	ANSELbits.ANS_i_VY1          = 1;
+	ANSELbits.ANS_i_VY2          = 1;
+	ANSELbits.ANS_o_ENGINEOFF    = 0;
+	TRISBbits.TRIS_i_NEUTRAL     = 1;
 	// Port B
 	TRISBbits.TRIS_i_LEFTARROW   = 1;
 	TRISBbits.TRIS_i_DOWNLIGHT   = 1;
@@ -130,6 +184,7 @@ void main(void) {
 	TRISBbits.TRIS_i_STARTBUTTON = 1;
 	TRISBbits.TRIS_i_ENGINEON    = 1;
 	TRISBbits.TRIS_i_DECOMPRESS  = 1;
+	TRISBbits.TRIS_i_POWEROFF    = 1;
 	TRISBbits.TRIS_o_STOPLIGHT   = 0;
 	TRISBbits.TRIS_i_ADDLIGHT    = 1;
 	TRISBbits.TRIS_i_LIGHTONOFF  = 1;
@@ -163,12 +218,24 @@ void main(void) {
 	CNPUBbits.CNPU_i_STARTBUTTON = 1;
 	CNPUBbits.CNPU_i_ENGINEON    = 1;
 	CNPUBbits.CNPU_i_DECOMPRESS  = 1;
+	CNPUBbits.CNPU_i_POWEROFF    = 1;
 	CNPUBbits.CNPU_i_ADDLIGHT    = 1;
 	CNPUBbits.CNPU_i_LIGHTONOFF  = 1;
 	
 	
+	//
+	// A/D converter settings....
+	//
+	ADCON0bits.ADON = 1;          // ADC enabling
+	ADCON1bits.ADCS = 0b111;      // Prescaler set to 128 for an adequate scan frequency
+	ADCON1bits.ADFM = 1;          // Right data alingment. A/D returns 10-bit integer
+	ADCON1bits.VCFG0 = 0;         // Imposta la tensione di riferimento bassa su Vss
+	ADCON1bits.VCFG1 = 0;         // Imposta la tensione di riferimento alta su Vdd
+
+	
+	//
 	// Starting conditions
-	LATBbits.LAT
+	//
 	LATBbits.LAT_o_RIGHTARROW  = 0;
 	LATBbits.LAT_o_LEFTARROW   = 0;
 	LATBbits.LAT_o_DOWNLIGHT   = 0;
@@ -177,14 +244,100 @@ void main(void) {
 	LATBbits.LAT_o_HORN        = 0;
 	LATBbits.LAT_o_KEEPALIVE   = 0;  // [!] IMPORTANT!
 	LATBbits.LAT_o_STARTENGINE = 0;
+	LATBbits.LAT_o_ENGINEOFF   = 1;
+
+
 
 	while (loop) {
-		//
-		// Resistor keys evaluation
-		//
+		if (ready_flag == 0) {
+			//
+			// Resistor keys evaluation
+			//
+			if (
+				abs(ADC_read(_i_VX1) - ADC_read(_i_VY1)) < V_TOLERANCE &&
+				abs(ADC_read(_i_VX2) - ADC_read(_i_VY2)) < V_TOLERANCE 
+			) {
+				// The keyword has been authenicated, you can unplug it
+				LATBbits.LAT_o_KEEPALIVE = 1;
+				ready_flag = 1;
+			}
+		
+		} else if (LATBbits.LAT_i_POWEROFF == 0) {
+			//
+			// Security POWER-OFF
+			//
+			LATBbits.LAT_o_KEEPALIVE = 0;
+			ready_flag = 0;
+			decompress_flag = 0;
+			LATBbits.LAT_o_ENGINEOFF = 1;
+		
+		} else {
+			//
+			// Lights and horn
+			//
+			LATBbits.LAT_o_UPLIGHT     = ! LATBbits.LAT_i_UPLIGHT;
+			LATBbits.LAT_o_DOWNLIGHT   = ! LATBbits.LAT_i_DOWNLIGHT;
+			LATBbits.LAT_o_HORN        = ! LATBbits.LAT_i_HORN;
+			LATBbits.LAT_o_ADDLIGHT    = ! LATBbits.LAT_i_ADDLIGHT;
+			LATBbits.LAT_o_BRAKESWITCH = ! LATBbits.LAT_i_BRAKESWITCH;
+			LATBbits.LAT_o_NEUTRAL     = ! LATBbits.LAT_i_NEUTRAL;
 
+			if (LATBbits.LAT_i_LEFTARROW  == 0) {
+				LATBbits.LAT_o_LEFTARROW = blink();
+				LATBbits.LAT_o_RIGHTARROW = 0;
 
+			} else if (LATBbits.LAT_i_RIGHTARROW == 0) {
+				LATBbits.LAT_o_RIGHTARROW = blink();
+				LATBbits.LAT_o_LEFTARROW = 0;
 
+			else {
+				LATBbits.LAT_o_RIGHTARROW = 0;
+				LATBbits.LAT_o_LEFTARROW = 0;
+			}
+			
+			// Is the gearbox in neutral position?
+			if (LATBbits.LAT_i_NEUTRAL == 0) neutral_flag = 1;
+
+			if (LATBbits.LAT_i_ENGINEON == 0) {
+				//
+				// "_i_ENGINEON" is enablen (0) when the switch placed in the right hand-bar is set to "RUN".
+				// In different case, It stop the engine and does not allow the motorbike engine to start or
+				// re-start.
+				//
+
+				LATBbits.LAT_o_ENGINEOFF = 0;
+
+				// Decompressor sensor management
+				if (LATBbits.LAT_i_DECOMPRESS  == 0) decompress_flag = 1;
+				
+				if (decompress_flag && neutral_flag && LATBbits.LAT_i_STARTBUTTON == 0)
+					engstart_flag = 1;
+					decompress_flag = 0;
+					LATBbits.LAT_o_STARTENGINE = 1; // START!!
+				}
+
+			} else {
+				decompress_flag = 0;            // reset decompressor status
+				LATBbits.LAT_o_ENGINEREADY = 0; // turn-off the led indicator
+				LATBbits.LAT_o_ENGINEOFF = 1;   // STOP the motorbike engine using CDI
+			}
+
+			if (engstart_flag) {
+				// The electric starter is running...
+				if (LATBbits.LAT_i_STARTBUTTON == 0)
+					// The button is still pressed
+					startButtonDebouncing = BUTTON_DEBOUNC;
+				else
+					// Probably the button has been released
+					startButtonDebouncing--;
+
+				if (startButtonDebouncing == 0) {
+					// OK the start button has been released
+					LATBbits.LAT_o_STARTENGINE = 0;
+					engstart_flag = 0;
+				}
+			}
+		}
 	}
 
 	return;
