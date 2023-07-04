c8f4ba28d167e67c96279a92cef5b207638fba5e
Silvano Catinella <catinella@yahoo.com>
2023-07-04 18:34:38 +0200

    First implementation

--- firmware/tests/test0-mbesSelector.c
+++ firmware/tests/test0-mbesSelector.c
@@ -12,7 +12,10 @@
 // Author: Silvano Catinella <catinella@yahoo.com>
 //
 // Description:
-//	
+//	This is the interactive simplest test for the mbesSelector module.
+//	To execute the test, correctly, you have to run in another terminal (or shell) the command printed on display after the
+//	test file execution. This command will open a virtual cockpit with switch and buttons, use it to verify the correct
+//	selectors management performed by the mbesSelector module
 //
 // License:
 //	Copyright (C) 2023 Silvano Catinella <catinella@yahoo.com>
@@ -28,3 +31,42 @@
 //		<https://www.gnu.org/licenses/gpl-3.0.txt>.
 //
 ------------------------------------------------------------------------------------------------------------------------------*/
+#define MOCK 1
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <mbesMock.h>
+#include <mbesSelector.h>
+
+bool loop = true;
+
+void sigHandler (int signum) {
+	printf("[!] I received a sig-%d\n", signum);
+	loop = false;
+	return;
+}
+
+int main() {
+
+	struct mbesSelector A1, A2, B2, B3, D3, D4;
+
+	mbesSelector_init(&A1, BUTTON,      "A1");
+	mbesSelector_init(&A2, BUTTON,      "A2");
+	mbesSelector_init(&B2, HOLDBUTTON,  "B2");
+	mbesSelector_init(&B3, HOLDBUTTON,  "B3");
+	mbesSelector_init(&D3, SWITCH,      "D3");
+	mbesSelector_init(&D4, SWITCH,      "D4");
+
+	signal(SIGTERM, sigHandler);
+	signal(SIGINT,  sigHandler);
+
+	while (loop) {
+	
+		usleep(10000);
+	}
+
+	return(0);
+}
