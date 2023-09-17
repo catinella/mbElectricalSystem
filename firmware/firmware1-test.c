4baa3278f9d661dd5c80dd6e76928d55b350f3f3
Silvano Catinella <catinella@yahoo.com>
2023-09-17 09:14:14 +0200

    New basic test

---
+++ firmware/firmware1-test.c
@@ -0,0 +1,51 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//
+//   __  __       _             _     _ _          _____ _           _        _           _   ____            _                 
+// |  \/  | ___ | |_ ___  _ __| |__ (_) | _____  | ____| | ___  ___| |_ _ __(_) ___ __ _| | / ___| _   _ ___| |_ ___ _ __ ___  
+// | |\/| |/ _ \| __/ _ \| '__| '_ \| | |/ / _ \ |  _| | |/ _ \/ __| __| '__| |/ __/ _` | | \___ \| | | / __| __/ _ \ '_ ` _ \ 
+// | |  | | (_) | || (_) | |  | |_) | |   <  __/ | |___| |  __/ (__| |_| |  | | (_| (_| | |  ___) | |_| \__ \ ||  __/ | | | | |
+// |_|  |_|\___/ \__\___/|_|  |_.__/|_|_|\_\___| |_____|_|\___|\___|\__|_|  |_|\___\__,_|_| |____/ \__, |___/\__\___|_| |_| |_|
+//                                                                                                 |___/                       
+//
+// File:   firmware1-test.c
+//
+// Author: Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//	This is another very simple test. It uses the project's library mbesUtitities to flash the selected PIN, alternately
+//	
+//
+// License:
+//	Copyright (C) 2023 Silvano Catinella <catinella@yahoo.com>
+//
+//	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+//	License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+//	version.
+//
+//	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License along with this program. If not, see
+//		<https://www.gnu.org/licenses/gpl-3.0.txt>.
+//
+------------------------------------------------------------------------------------------------------------------------------*/
+#include <mbesPinsMap.h>
+#include <mbesUtitities.h>
+#include <avr/io.h>
+
+
+int main() {
+	char    *pin = o_KEEPALIVE;
+	
+	CLKPR = (1 << CLKPCE);  // Clock prescaling enabling
+	CLKPR = 0;              // It sets the prescaler to 1
+	
+	while (1) {
+		setPinValue (pin, 1);
+		_delay_ms(1000);
+		setPinValue (pin, 0);
+		_delay_ms(1000);
+	}
+	
+	return(0);
+}
