2ccd0a4c15ae62d044b42010050fefe662d6d1f9
Silvano Catinella <catinella@yahoo.com>
2023-10-29 09:53:38 +0100

    Module for ADC operations

---
+++ firmware/mbesADCengine.h
@@ -0,0 +1,38 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//
+//  __  __       _             _     _ _          _____ _           _        _           _   ____            _                 
+// |  \/  | ___ | |_ ___  _ __| |__ (_) | _____  | ____| | ___  ___| |_ _ __(_) ___ __ _| | / ___| _   _ ___| |_ ___ _ __ ___  
+// | |\/| |/ _ \| __/ _ \| '__| '_ \| | |/ / _ \ |  _| | |/ _ \/ __| __| '__| |/ __/ _` | | \___ \| | | / __| __/ _ \ '_ ` _ \
+// | |  | | (_) | || (_) | |  | |_) | |   <  __/ | |___| |  __/ (__| |_| |  | | (_| (_| | |  ___) | |_| \__ \ ||  __/ | | | | |
+// |_|  |_|\___/ \__\___/|_|  |_.__/|_|_|\_\___| |_____|_|\___|\___|\__|_|  |_|\___\__,_|_| |____/ \__, |___/\__\___|_| |_| |_|
+//                                                                                                 |___/                       
+//
+// File:   mbesADCengine.h
+//
+// Author: Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//	Thif file contains useful functions to convert analog inputs in digital numbers
+//
+// License:
+//	Copyright (C) 2023 Silvano Catinella <catinella@yahoo.com>
+//
+//	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+//	License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+//	version.
+//
+//	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License along with this program. If not, see
+//		<https://www.gnu.org/licenses/gpl-3.0.txt>.
+//
+------------------------------------------------------------------------------------------------------------------------------*/
+#ifndef MBESADCENGINE
+#define MBESADCENGINE
+
+#include <mbesUtilities.h>
+
+uint16_t ADC_read (const char *code);
+
+#endif
