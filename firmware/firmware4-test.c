5d7564c234da9a788ff749416b4fb671f4c04b4c
Silvano Catinella <silvano.catinella@fimer.com>
2023-10-30 23:33:01 +0100

    Test for MCP23008 chip

---
+++ firmware/firmware4-test.c
@@ -0,0 +1,41 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//
+//   __  __       _             _     _ _          _____ _           _        _           _   ____            _                 
+// |  \/  | ___ | |_ ___  _ __| |__ (_) | _____  | ____| | ___  ___| |_ _ __(_) ___ __ _| | / ___| _   _ ___| |_ ___ _ __ ___  
+// | |\/| |/ _ \| __/ _ \| '__| '_ \| | |/ / _ \ |  _| | |/ _ \/ __| __| '__| |/ __/ _` | | \___ \| | | / __| __/ _ \ '_ ` _ \ 
+// | |  | | (_) | || (_) | |  | |_) | |   <  __/ | |___| |  __/ (__| |_| |  | | (_| (_| | |  ___) | |_| \__ \ ||  __/ | | | | |
+// |_|  |_|\___/ \__\___/|_|  |_.__/|_|_|\_\___| |_____|_|\___|\___|\__|_|  |_|\___\__,_|_| |____/ \__, |___/\__\___|_| |_| |_|
+//                                                                                                 |___/                       
+//
+// File:   firmware4-test.c
+//
+// Author: Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//	This test has been developped to ckeck for the I/O extension chip on I2C BUS.
+//
+// License:
+//	Copyright (C) 2023 Silvano Catinella <catinella@yahoo.com>
+//
+//	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+//	License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+//	version.
+//
+//	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License along with this program. If not, see
+//		<https://www.gnu.org/licenses/gpl-3.0.txt>.
+//
+------------------------------------------------------------------------------------------------------------------------------*/
+#include <mbesHwConfig.h>
+#include <mbesI2C.h>
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+
+int main() {
+	
+	return(0);
+}
