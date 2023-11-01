52840104dc69eb3148fe9bb69ebe2bbfe4923693
Silvano Catinella <silvano.catinella@fimer.com>
2023-11-01 17:10:20 +0100

    first implementation

--- firmware/firmware4-test.c
+++ firmware/firmware4-test.c
@@ -12,7 +12,12 @@
 // Author: Silvano Catinella <catinella@yahoo.com>
 //
 // Description:
-//	This test has been developped to ckeck for the I/O extension chip on I2C BUS.
+//	This test has been developped to ckeck for the I/O extension chip MCP23008-XP. This device is connected to the ATmega16
+//	using I2C BUS.
+//
+//	This test will read the MCP23008-XP's GP0 (pin-10) status and will set the GP3 (pin-13) at the same status.
+//	The MCP23008-XP's address is 0000:0000
+//
 //
 // License:
 //	Copyright (C) 2023 Silvano Catinella <catinella@yahoo.com>
@@ -35,7 +40,68 @@
 #include <util/delay.h>
 #include <stdio.h>
 
+//
+// I2C I/O extender MCP23008XP's regirters
+//
+#define IODIR_ADDR 0x00
+#define GPIO_ADDR  0x09
+#define GPPU_ADDR  0x06
+
+
 int main() {
+	uint8_t regValue = 0;
+ 
+	I2C_init();
 	
+	// "IODIR" register selecting
+	I2C_Start();
+	I2C_Write(0);                 // LSB=0 --> writing operation
+	I2C_Write(GPIO_ADDR);
+	
+	// "IODIR" register reading
+	I2C_Start();
+	I2C_Write(1);                 // LSB=1 --> reading operation
+	regValue = I2C_Read(I2C_NACK);
+	
+	// "IODIR" register changing
+	regValue &= ~(1 << 3);          // GP3 (bit0 --> OUTPUT)
+	regValue |=  (1 << 0);          // GP0 (bit1 --> INPUT)
+		
+	// "IODIR" register saving
+	I2C_Start();
+	I2C_Write(0);               // LSB=0 --> writing operation
+	I2C_Write(regValue);
+
+	I2C_Stop();
+
+		
+	while(1) {
+		
+		// "GPIO" register selecting
+		I2C_Start();
+		I2C_Write(0);                   // LSB=0 --> writing operation
+		I2C_Write(GPIO_ADDR);
+		
+		// "GPIO" register reading
+		I2C_Start();
+		I2C_Write(1);                   // LSB=1 --> reading operation
+		regValue = I2C_Read(I2C_NACK);
+		
+		// "GPIO" changing
+		if ((regValue & 1) == 0)        // GP0 == 0 ?
+			regValue &= ~(1 << 3);    // GP3 = 0
+		else
+			regValue |= (1 << 3);     // GP3 = 1
+
+		// "IODIR" register saving
+		I2C_Start();
+		I2C_Write(0);                    // LSB=0 --> writing operation
+		I2C_Write(regValue);
+
+		I2C_Stop();
+
+		_delay_ms(500);
+	}
+			
 	return(0);
 }
