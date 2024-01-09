e329f40a309ce7e600ec3bb14844b8f0c60acab2
Silvano Catinella <silvano.catinella@fimer.com>
2024-01-09 13:24:37 +0100

    Revert "mbesSelector.c"
    This reverts commit a8176bf89c651d1321b9d5979643efcaf213ac38.

--- firmware/mbesSelector.h
+++ firmware/mbesSelector.h
@@ -54,8 +54,8 @@ struct mbesSelector {
 //------------------------------------------------------------------------------------------------------------------------------
 //                                                  F U N C T I O N S 
 //------------------------------------------------------------------------------------------------------------------------------
-uint8_t mbesSelector_init    (struct mbesSelector *item, selectorType type, const char *pin);
-bool    mbesSelector_get     (struct mbesSelector item);
-uint8_t mbesSelector_update  (struct mbesSelector *item);
+void mbesSelector_init    (struct mbesSelector *item, selectorType type, const char *pin);
+bool mbesSelector_get     (struct mbesSelector item);
+void mbesSelector_update  (struct mbesSelector *item);
 
 #endif
