d2ea1cf3f88c069a87abb276cefc5ce0fc68f240
Warlock <warlock@archimede.localdomain>
2024-01-14 12:17:39 +0100

    Procedure for automated debug-symbols management

---
+++ firmware/create_MakefileDgbSyms.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MKCFILE="Makefile.dbgsyms"
+
+symsList=" $(sed -n 's/^.* \(MBES_[^_]\+_DEBUG\) .*/\1/p' *.[ch] |sort |uniq)"
+
+echo "DEBUG ?= 0"            > $MKCFILE
+for symbol in $symsList
+do
+	echo "$symbol ?= 0"    >> $MKCFILE
+done
+echo -e "\n"                 >> $MKCFILE
+
+
+echo "DBGSYMBOLS = \\"                            >> $MKCFILE
+echo -e "\t\"-DDEBUG=\$(DEBUG) \\"                >> $MKCFILE
+for symbol in $symsList
+do
+	echo -e "\t\"-D${symbol}=\$($symbol)\" \\"  >> $MKCFILE
+done
+echo -e "\n"                                      >> $MKCFILE
+
+echo "$MKCFILE file created"
