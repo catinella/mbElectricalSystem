9f9647bb9400ac22e002b4dd65954c2764605275
Warlock <warlock@archimede.localdomain>
2024-02-18 16:19:49 +0100

    Unrecognized char trouble fixed
    The integration tests have shown a problem on the first received char. This invisible one, make  (often but not
    always) fail the pin definitioon syntax matching. I was unable to discover this character, in Perl, (Perl is not C), so
    I modified the regex to be a more generic one. It is just a workaround. But, at the moment, it functions very well

--- firmware/tools/debugConsole.pl
+++ firmware/tools/debugConsole.pl
@@ -201,7 +201,7 @@ if (not defined($serialPort) or $serialPort eq "") {
 			$_ = <$spFH>;
 			if (defined($_) and $_ ne "\n") {
 				chomp;
-				if (/^([A-Z0-9][0-9]) *: *([0-9]+)$/) {
+				if (/^[^A-Za-z\s0-9]*([A-Z0-9][0-9]) *: *([0-9]+)\s*$/) {
 					# New pin-setting
 					$signalsDB{$1} = $2 
 	
