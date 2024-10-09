af801075f880624eca21f7838fa6397e615e2920
Silvano Catinella <catinella@yahoo.com>
2024-10-09 17:04:39 +0200

    Simple test to verify the iInputInterface module

---
+++ firmware-esp32/main/test06.c
@@ -0,0 +1,128 @@
+
+/*------------------------------------------------------------------------------------------------------------------------------
+//
+//  __  __       _             _     _ _          _____ _           _        _           _   ____            _                 
+// |  \/  | ___ | |_ ___  _ __| |__ (_) | _____  | ____| | ___  ___| |_ _ __(_) ___ __ _| | / ___| _   _ ___| |_ ___ _ __ ___  
+// | |\/| |/ _ \| __/ _ \| '__| '_ \| | |/ / _ \ |  _| | |/ _ \/ __| __| '__| |/ __/ _` | | \___ \| | | / __| __/ _ \ '_ ` _ \
+// | |  | | (_) | || (_) | |  | |_) | |   <  __/ | |___| |  __/ (__| |_| |  | | (_| (_| | |  ___) | |_| \__ \ ||  __/ | | | | |
+// |_|  |_|\___/ \__\___/|_|  |_.__/|_|_|\_\___| |_____|_|\___|\___|\__|_|  |_|\___\__,_|_| |____/ \__, |___/\__\___|_| |_| |_|
+//                                                                                                 |___/                       
+//
+// File:   test06.c
+//
+// Author: Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//	This simple test has been written to verify the iInputInterface module and requires the following circuit.
+//	The button-b (i_CONF2) has been configured as simple button, so everytime you will push it, the led-b will be on.
+//	The button-a (i_CONF1) is configured ad HOLD-ON button. So, it will require a click to turn on the led-a, and another
+//	click to turn it off
+//
+//	Vcc ------------------+-----+-----+------------------------------------------
+//	                      |     |     |
+//	                    +-+-+ +-+-+   |  +-------+
+//	                    |10K| |10K|   +--+       |    +---------+
+//	                    |OHM| |OHM|      |       +--->|   USB   |       
+//	                    +-+-+ +-+-+      |       |    | Console |
+//	              _--_    |     |        |       |    +---------+
+//	          +---O  O----+--------------+       |
+//	          | (button-A)      |        |       |              +--------+
+//	          |   _==_          |        |  MCU  +---[LED A]----+ 2K Ohm +---+
+//	          +---O  O----------+--------+       |              +--------+   |
+//	          | (button-B)               |       |                           |
+//	          |                          |       |              +--------+   |
+//	          |                       +--+       +---[LED B]----+ 2K Ohm +---+
+//	          |                       |  |       |              +--------+   |
+//	          |                       |  +-------+                           |
+//	GND ------+-----------------------+--------------------------------------+---
+
+//	This software has been developed for ESP-IDF v5.4 and ESP32-S2-DevKitM-1
+//
+// License:
+//	Copyright (C) 2023 Silvano Catinella <catinella@yahoo.com>
+//
+//	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+//	License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+//	version.
+//
+//	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License along with this program. If not, see
+//		<https://www.gnu.org/licenses/gpl-3.0.txt>.
+//
+------------------------------------------------------------------------------------------------------------------------------*/
+
+// Platform dependent libs
+#include "esp_timer.h"
+#include "driver/gpio.h"
+
+// Higher level libs
+#include <stdio.h>
+#include "esp_log.h"
+#include "esp_err.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/portmacro.h>
+#include <freertos/task.h>
+
+// Project's libs
+#include <mbesPinsMap.h>
+#include <iInputInterface.h>
+
+
+
+void app_main(void) {
+	
+	
+	if (iInputInterface_init() != IINPUTIF_SUCCESS)
+		// ERRPR!
+		ESP_LOGE(__FUNCTION__, "iInputInterface module initialization failed");
+		
+	else {
+		uint8_t       btnA = 0; btnB = 0;
+		gpio_config_t ledA, ledB;
+		
+		//
+		// PIN configuration
+		//
+		ledA.intr_type    = GPIO_INTR_DISABLE;
+		ledA.mode         = GPIO_MODE_OUTPUT;
+		ledA.pin_bit_mask = (1ULL << o_KEEPALIVE);
+		ledA.pull_down_en = GPIO_PULLDOWN_DISABLE;
+		ledA.pull_up_en   = GPIO_PULLDOWN_DISABLE;
+	
+		ledB.intr_type    = GPIO_INTR_DISABLE;
+		ledB.mode         = GPIO_MODE_OUTPUT;
+		ledB.pin_bit_mask = (1ULL << o_NEUTRAL);
+		ledB.pull_down_en = GPIO_PULLDOWN_DISABLE;
+		ledB.pull_up_en   = GPIO_PULLDOWN_DISABLE;
+		
+		if (gpio_config(&ledA) != ESP_OK || gpio_config(&ledB) != ESP_OK)
+			// ERROR!
+			ESP_LOGE(__FUNCTION__, "ERROR! LED GPIO configuration failed");
+		
+		else if (	
+			iInputInterface_new(&btnA, BUTTON,     i_CONF1) != IINPUTIF_SUCCESS ||
+			iInputInterface_new(&btnB, HOLDBUTTON, i_CONF2) != IINPUTIF_SUCCESS
+		)
+			// ERRPR!
+			ESP_LOGE(__FUNCTION__, "Object creation failed");
+		
+		else {
+			bool statA = false, statB;
+			while (1) {
+				if (
+					iInputInterface_get(btnA, &statA) != IINPUTIF_SUCCESS ||
+					iInputInterface_get(btnA, &statB) != IINPUTIF_SUCCESS
+				)
+					// ERROR!
+					ESP_LOGE(__FUNCTION__, "ERROR! buttons' status reading failed");
+				
+				else {
+					gpio_set_level(o_KEEPALIVE, statA ? 1 : 0);
+					gpio_set_level(o_KEEPALIVE, statB ? 1 : 0);
+				}
+			}
+		}
+	}
+}
